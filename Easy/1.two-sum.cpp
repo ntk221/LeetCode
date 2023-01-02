00ba2a66134ac2e803df6903d1fee0b906c9286c
ntk221 <nittakdub@gmail.com>
2023-01-03 00:04:52 +0900

    refactored

--- Easy/1.two-sum.cpp
+++ Easy/1.two-sum.cpp
@@ -13,14 +13,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans(2);
-        for(int i = 0; i < nums.size(); i++)
+        const int   len = nums.size();
+
+        for(int i = 0; i < len; i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            for (int j = i + 1; j < len; j++)
             {
                 if (nums[i] + nums[j] == target)
                 {
                     ans[0] = i;
                     ans[1] = j;
+                    return (ans);
                 }
             }
         }
